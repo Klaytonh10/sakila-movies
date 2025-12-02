85613760a78097a0e9f55feff757fcc15ca9b3f9
Klaytonh10 <klaytonh10@gmail.com>
2025-12-02 14:58:35 -0600

    DataManager and Main reconfigured

--- src/main/java/com/pluralsight/DataManager.java
+++ src/main/java/com/pluralsight/DataManager.java
@@ -1,7 +1,50 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class DataManager {
 
+    public ArrayList<Actor> getActors(String firstName, String lastName) {
 
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername("root");
+        dataSource.setPassword("yearup");
+
+        ArrayList<Actor> actors = new ArrayList<>();
+
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "select actor_id, first_name, last_name from actor where first_name = ? and last_name = ?"
+                );
+        ) {
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+
+            try (
+                    ResultSet resultSet = preparedStatement.executeQuery();
+            ) {
+
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("actor_id");
+                    String dbFirstName = resultSet.getString("first_name");
+                    String dbLastName = resultSet.getString("last_name");
+                    actors.add(new Actor(id, dbFirstName, dbLastName));
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return actors;
+    }
 
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,18 +1,15 @@
 package com.pluralsight;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
-
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static Scanner scanner = new Scanner(System.in);
-    private static String input = " ";
-
-    private static MysqlDataSource dataSource = new MysqlDataSource();
-
     public static void main(String[] args) {
+        DataManager dataManager = new DataManager();
+        ArrayList<Actor> actors = dataManager.getActors("Bob", "Fawcett");
 
-
+        for(Actor a : actors) {
+            System.out.println(a.getActorID() + " " + a.getFirstName() + " "+ a.getLastName());
+        }
     }
 }
